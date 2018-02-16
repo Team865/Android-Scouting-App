02a64a50466693132fa6b3108b1ba9b02cca7060
yuliu2016 <yuliu2016@users.noreply.github.com>
2018-02-15 23:51:49 -0500

    UI Feedback Tests

--- app/src/main/java/ca/warp7/android/scouting/AutoInputs.java
+++ app/src/main/java/ca/warp7/android/scouting/AutoInputs.java
@@ -1,14 +1,20 @@
 package ca.warp7.android.scouting;
 
 
+import android.content.Context;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Vibrator;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.Toast;
 
 
 public class AutoInputs extends Fragment
@@ -16,6 +22,8 @@ public class AutoInputs extends Fragment
 
     CheckBox lineCheck;
     TimedScoutingActivity activity;
+
+    int counter = 0;
 
 
     public AutoInputs(){
@@ -48,6 +56,8 @@ public class AutoInputs extends Fragment
         view.findViewById(R.id.exchange_attempt_button).setOnClickListener(this);
         view.findViewById(R.id.exchange_success_button).setOnClickListener(this);
 
+        counter = 0;
+
     }
 
     @Override
@@ -56,26 +66,58 @@ public class AutoInputs extends Fragment
 
             case R.id.scale_attempt_button:
                 activity.pushElapsed(Static.AUTO_SCALE_ATTEMPT);
+                getView().findViewById(R.id.msg_top).setVisibility(View.VISIBLE);
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        getView().findViewById(R.id.msg_top).setVisibility(View.INVISIBLE);
+                    }
+                }, 1000);
+
                 break;
 
             case R.id.scale_success_button:
                 activity.pushElapsed(Static.AUTO_SCALE_SUCCESS);
+                getView().findViewById(R.id.msg_buttom).setVisibility(View.VISIBLE);
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        getView().findViewById(R.id.msg_buttom).setVisibility(View.INVISIBLE);
+                    }
+                }, 1000);
                 break;
 
             case R.id.switch_attempt_button:
                 activity.pushElapsed(Static.AUTO_SWITCH_ATTEMPT);
+                Toast.makeText(activity, "Switch Attempt at x minutes x seconds",
+                        Toast.LENGTH_SHORT).show();
                 break;
 
             case R.id.switch_success_button:
                 activity.pushElapsed(Static.AUTO_SWITCH_SUCCESS);
+                final Button b = getView().findViewById(R.id.switch_success_button);
+                b.setTextColor(0xFFFFFFFF);
+                b.setBackgroundColor(0xFF3F51B5);
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        b.setBackgroundResource(android.R.drawable.btn_default);
+                        b.setTextColor(0xFF000000);
+                    }
+                }, 500);
                 break;
 
             case R.id.exchange_attempt_button:
                 activity.pushElapsed(Static.AUTO_EXCHANGE_ATTEMPT);
+                ((Vibrator) activity.getSystemService(Context.VIBRATOR_SERVICE))
+                        .vibrate(40);
                 break;
 
             case R.id.exchange_success_button:
                 activity.pushElapsed(Static.AUTO_EXCHANGE_ATTEMPT);
+                counter++;
+                ((Button) getView().findViewById(R.id.exchange_success_button))
+                        .setText("Exchange Success \n" + counter);
                 break;
 
             default:
