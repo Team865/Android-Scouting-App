06d8b31808a35ddcd4066e169d0bc5ab625d8017
yuliu2016 <yuliu2016@users.noreply.github.com>
2018-02-16 19:01:38 -0500

    v0.1 release commit

--- app/src/main/java/ca/warp7/android/scouting/AutoInputs.java
+++ app/src/main/java/ca/warp7/android/scouting/AutoInputs.java
@@ -1,11 +1,8 @@
 package ca.warp7.android.scouting;
 
 
-import android.content.Context;
-import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.Vibrator;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -14,7 +11,6 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
-import android.widget.Toast;
 
 
 public class AutoInputs extends Fragment
