8649f9f4a90dc4d7dbef53f14b796f7e86b43106
yuliu2016 <yuliu2016@users.noreply.github.com>
2018-08-14 21:12:02 +0800

    create manageddata class

--- app/src/main/java/ca/warp7/android/scouting/EntrySelectorActivity.java
+++ app/src/main/java/ca/warp7/android/scouting/EntrySelectorActivity.java
@@ -1,19 +1,22 @@
 package ca.warp7.android.scouting;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Spinner;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 
 public class EntrySelectorActivity extends AppCompatActivity {
+
+    ManagedData.MatchTable mMatchTable;
+    ListView mEntryList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,46 +24,39 @@ public class EntrySelectorActivity extends AppCompatActivity {
         setContentView(R.layout.activity_entry_selector);
         setSupportActionBar((Toolbar) findViewById(R.id.my_toolbar));
         setTitle("Entry Selector");
-
-        ListView lv = findViewById(R.id.entry_list);
         Spinner spinner = findViewById(R.id.board_spinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.board_choices, android.R.layout.simple_spinner_item);
-
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
-
-
+        mEntryList = findViewById(R.id.entry_list);
         try {
-            File mtf = new File(Specs.getSpecsRoot(), "match-table.csv");
-            String mt_content = readFile(mtf);
-            String[] split = mt_content.split("\n");
-            ArrayAdapter aa = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, split);
-            lv.setAdapter(aa);
-
+            mMatchTable = new ManagedData.MatchTable();
         } catch (IOException e) {
             e.printStackTrace();
             new AlertDialog.Builder(this)
                     .setTitle("An error occurred")
                     .setMessage(e.toString())
-                    .create()
-                    .show();
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            onBackPressed();
+                        }
+                    })
+                    .create().show();
         }
-    }
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                mEntryList.setAdapter(new ArrayAdapter<>(EntrySelectorActivity.this,
+                        android.R.layout.simple_list_item_1,
+                        mMatchTable.getTeamsArrayForBoard(position)));
+            }
 
-    private static String readFile(File f) throws IOException {
-        BufferedReader br = new BufferedReader(new FileReader(f));
-        StringBuilder sb = new StringBuilder();
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
 
-        String line = br.readLine();
-
-        while (line != null) {
-            sb.append(line);
-            sb.append("\n");
-            line = br.readLine();
-        }
-
-        br.close();
-        return sb.toString();
+            }
+        });
     }
 }
---
+++ app/src/main/java/ca/warp7/android/scouting/ManagedData.java
@@ -0,0 +1,68 @@
+package ca.warp7.android.scouting;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+class ManagedData {
+
+    static class MatchInfo {
+        private int[] mTeams;
+        private int mMatchNumber;
+
+        MatchInfo(String matchCSV) {
+            String[] split = matchCSV.split(",");
+            mMatchNumber = Integer.valueOf(split[0]);
+            mTeams = new int[6];
+            for (int i = 1; i < 7; i++) {
+                mTeams[i - 1] = Integer.valueOf(split[i]);
+            }
+        }
+
+        public int getTeamAt(int i) {
+            return mTeams[i];
+        }
+
+        public int[] getTeams() {
+            return mTeams;
+        }
+
+        public int getMatchNumber() {
+            return mMatchNumber;
+        }
+    }
+
+    static class MatchTable {
+        List<MatchInfo> mMatches = new ArrayList<>();
+
+
+        MatchTable() throws IOException {
+
+            File mtf = new File(Specs.getSpecsRoot(), "match-table.csv");
+            BufferedReader br = new BufferedReader(new FileReader(mtf));
+
+            br.readLine(); // Removes the headers line
+            String line = br.readLine();
+
+            while (line != null) {
+                mMatches.add(new MatchInfo(line));
+                line = br.readLine();
+            }
+
+            br.close();
+        }
+
+        String[] getTeamsArrayForBoard(int board) {
+            String[] result = new String[mMatches.size()];
+            for (int i = 0; i < mMatches.size(); i++) {
+                result[i] = String.valueOf(mMatches.get(i).getTeamAt(board));
+            }
+            return result;
+        }
+    }
+
+
+}
