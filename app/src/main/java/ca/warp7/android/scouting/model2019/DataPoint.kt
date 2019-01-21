02aa4ad5dd8e11e93ba959bd9e10ea6947f7520b
yuliu2016 <22801773+yuliu2016@users.noreply.github.com>
2019-01-21 17:48:51 -0500

    datapoint now implements iterable

--- app/src/main/java/ca/warp7/android/scouting/model2019/DataPoint.kt
+++ app/src/main/java/ca/warp7/android/scouting/model2019/DataPoint.kt
@@ -1,3 +1,5 @@
 package ca.warp7.android.scouting.model2019
 
-data class DataPoint(val type: Byte, val value: Byte, val time: Byte)
+data class DataPoint(val type: Byte, val value: Byte, val time: Byte): Iterable<Byte>{
+    override fun iterator() = byteArrayOf(type, value, time).iterator()
+}
--- app/src/main/java/ca/warp7/android/scouting/model2019/Entry.kt
+++ app/src/main/java/ca/warp7/android/scouting/model2019/Entry.kt
@@ -38,30 +38,35 @@ data class Entry(
     val board: Board,
     val timestamp: Int,
     val timeSource: () -> Byte,
-    val isTiming: Boolean = true
+    val data: MutableList<DataPoint> = mutableListOf(),
+    val isTiming: Boolean = false,
+    var comments: String = ""
 ) {
 
-    private val dataPoints: MutableList<DataPoint> = mutableListOf()
-    private val hexTimestamp = Integer.toHexString(timestamp)
 
-    var comments: String = ""
+    private val hexTimestamp = Integer.toHexString(timestamp)
+    private val encodedData: String get() = Base64.encodeToString(data.flatten().toByteArray(), Base64.DEFAULT)
+    private val comments1 get() = comments.replace("[^A-Za-z0-9 ]".toRegex(), "_")
+    private val scout1 get() = scout.replace("[^A-Za-z0-9 ]".toRegex(), "_")
+    val encodedString: String get() = "$match:$team:$scout1:${board.name}:$hexTimestamp:$encodedData:$comments1"
 
     /**
      * Get the maximum index of the datum recorded before or equal the current time,
-     * or the last item in the datapoints
+     * or the last item in the data points
      */
     private val nextIndex: Int
         get() {
-            if (!isTiming) return dataPoints.size
+            if (!isTiming) return data.size
             val relTime = timeSource.invoke()
-            var index = 0 // find the maximum index that is less than current time
-            while (index < dataPoints.size && dataPoints[index].time <= relTime) {
-                index++
-            }
+            var index = 0
+            while (index < data.size && data[index].time <= relTime) index++
             return index
         }
 
-    fun add(dataPoint: DataPoint) = dataPoints.add(index = nextIndex, element = dataPoint)
+    /**
+     * Adds a data point to the entry
+     */
+    fun add(dataPoint: DataPoint) = data.add(index = nextIndex, element = dataPoint)
 
     /**
      * Performs an undo action on the data stack
@@ -69,38 +74,20 @@ data class Entry(
      * @return the data constant(metrics) of the datum being undone, or null
      * if nothing can be undone
      */
-    fun undo() = nextIndex.let { if (it == 0) null else dataPoints.removeAt(it) }
+    fun undo() = nextIndex.let { if (it == 0) null else data.removeAt(it) }
 
     /**
      * Gets the count of a specific data type, excluding undo
      */
-    fun count(dataType: Byte) = dataPoints.subList(0, nextIndex).filter { it.type == dataType }.size
+    fun count(type: Byte) = data.subList(0, nextIndex).filter { it.type == type }.size
 
     /**
      * Gets the last recorded of a specific data type, excluding undo
      */
-    fun lastValue(dataType: Byte) = dataPoints.subList(0, nextIndex).lastOrNull { it.type == dataType }
+    fun lastValue(type: Byte) = data.subList(0, nextIndex).lastOrNull { it.type == type }
 
     /**
      * Cleans out data that have been undone
      */
-    fun focused(dataType: Byte) =
-        timeSource.invoke().let { t -> dataPoints.any { it.type == dataType && it.time == t } }
-
-    private val encodedDataPoints: String
-        get() = Base64.encodeToString(dataPoints
-            .flatMap { byteArrayOf(it.type, it.value, it.time).asIterable() }.toByteArray(), Base64.DEFAULT)
-
-    private val constrainedComments
-        get() = comments
-            .let { if (it.length > 63) it.substring(0..64) else it }
-            .replace("[^A-Za-z0-9 ]".toRegex(), "_")
-
-    private val constrainedScout
-        get() = board.name
-            .let { if (it.length > 15) it.substring(0..16) else it }
-            .replace("[^A-Za-z0-9 ]".toRegex(), "_")
-
-    val encodedString: String
-        get() = "$match:$team:$scout:${board.name}:$hexTimestamp:$encodedDataPoints:$constrainedComments"
+    fun focused(type: Byte) = timeSource.invoke().let { t -> data.any { it.type == type && it.time == t } }
 }
