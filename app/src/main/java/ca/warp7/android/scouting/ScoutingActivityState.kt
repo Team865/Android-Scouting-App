29e6f58236632b2179cecfe2d620eba70d90c52c
yuliu2016 <22801773+yuliu2016@users.noreply.github.com>
2019-01-22 17:37:20 -0500

    add ScoutingActivityState

---
+++ app/src/main/java/ca/warp7/android/scouting/ScoutingActivityState.kt
@@ -0,0 +1,5 @@
+package ca.warp7.android.scouting
+
+enum class ScoutingActivityState {
+    WaitingToStart, TimedScouting, Pausing
+}
--- app/src/main/java/ca/warp7/android/scouting/V5Activity.kt
+++ app/src/main/java/ca/warp7/android/scouting/V5Activity.kt
@@ -15,6 +15,8 @@ import android.widget.SeekBar
 import android.widget.TextView
 import ca.warp7.android.scouting.abstraction.AbstractActionVibrator
 import ca.warp7.android.scouting.components.ScoutingTabsPagerAdapter
+import ca.warp7.android.scouting.constants.Constants.kFadeDuration
+import ca.warp7.android.scouting.constants.Constants.kTimerLimit
 import ca.warp7.android.scouting.constants.ID
 import ca.warp7.android.scouting.res.ManagedPreferences
 import ca.warp7.android.scouting.v5.boardfile.Boardfile
@@ -30,29 +32,62 @@ abstract class V5Activity : AppCompatActivity(), ScoutingActivityBase {
     override var entry: MutableEntry? = null
     override var timeEnabled: Boolean = true
 
-    private lateinit var mTimerStatus: TextView
-    private lateinit var mTimeProgress: ProgressBar
-    private lateinit var mTimeSeeker: SeekBar
-    private lateinit var mStartButton: TextView
-    private lateinit var mPlayAndPauseImage: ImageButton
-    private lateinit var mUndoAndNowImage: ImageButton
-    private lateinit var mPlayAndPauseView: ViewGroup
-    private lateinit var mUndoAndNowView: ViewGroup
-    private lateinit var mPlayAndPauseText: TextView
-    private lateinit var mUndoAndNowText: TextView
-    private lateinit var mPager: ViewPager
-    private lateinit var mPagerAdapter: ScoutingTabsPagerAdapter
+    private lateinit var timerStatus: TextView
+    private lateinit var timeProgress: ProgressBar
+    private lateinit var timeSeeker: SeekBar
+    private lateinit var startButton: TextView
+    private lateinit var playAndPauseImage: ImageButton
+    private lateinit var undoAndNowImage: ImageButton
+    private lateinit var playAndPauseView: ViewGroup
+    private lateinit var undoAndNowView: ViewGroup
+    private lateinit var playAndPauseText: TextView
+    private lateinit var undoAndNowText: TextView
+    private lateinit var pager: ViewPager
+    private lateinit var pagerAdapter: ScoutingTabsPagerAdapter
 
     private lateinit var preferences: ManagedPreferences
     private lateinit var boardfile: Boardfile
+
+    private var activityState = ScoutingActivityState.WaitingToStart
+    private var relativeTime = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         handler = Handler()
         preferences = ManagedPreferences(this)
         boardfile = File(intent.getStringExtra(ID.IntentBoardfile)).toBoardfile()
+        setTheme(R.style.AppTheme)
+        setContentView(R.layout.activity_scouting)
+        setSupportActionBar(findViewById(R.id.my_toolbar))
+        timerStatus = findViewById(R.id.timer_status)
+        startButton = findViewById(R.id.start_timer)
+        startButton.elevation = 4f
+        playAndPauseImage = findViewById(R.id.play_pause_image)
+        undoAndNowImage = findViewById(R.id.undo_now_image)
+        playAndPauseView = findViewById(R.id.play_pause_container)
+        undoAndNowView = findViewById(R.id.undo_now_container)
+        playAndPauseText = findViewById(R.id.play_pause_text)
+        undoAndNowText = findViewById(R.id.undo_now_text)
+        timeProgress = findViewById(R.id.time_progress)
+        timeSeeker = findViewById(R.id.time_seeker)
+        timeProgress.max = kTimerLimit
+        timeProgress.progress = 0
+        timeSeeker.max = kTimerLimit
+        timeSeeker.progress = 0
+        timeSeeker.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                if (fromUser && activityState == ScoutingActivityState.Pausing) {
+                    relativeTime = progress
+                    // FIXME updateTimerStatusAndProgressBar()
+                    // FIXME updateAdjacentTabStates()
+                }
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) = Unit
+            override fun onStopTrackingTouch(seekBar: SeekBar) = Unit
+        })
     }
 
-    val mAlphaAnimationIn: Animation = AlphaAnimation(0.0f, 1.0f)
-    val mAlphaAnimationOut: Animation = AlphaAnimation(1.0f, 0.0f)
+    private val mAlphaAnimationIn: Animation = AlphaAnimation(0.0f, 1.0f).apply { duration = kFadeDuration.toLong() }
+    private val mAlphaAnimationOut: Animation = AlphaAnimation(1.0f, 0.0f).apply { duration = kFadeDuration.toLong() }
 }
