b38fc8029b03a0932951fd7cf5ea0a4abd851395
yuliu2016 <22801773+yuliu2016@users.noreply.github.com>
2019-01-27 15:58:16 -0500

    add V5MainActivity

---
+++ app/src/main/java/ca/warp7/android/scouting/V5MainActivity.kt
@@ -0,0 +1,40 @@
+package ca.warp7.android.scouting
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Button
+import ca.warp7.android.scouting.v5.V5Activity
+
+class V5MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_v5_main)
+        setSupportActionBar(findViewById(R.id.my_toolbar))
+        supportActionBar?.title = "Humber College Event"
+
+        findViewById<Button>(R.id.v5_starter).setOnClickListener {
+            startActivity(Intent(this, V5Activity::class.java))
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.v5_main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return item?.itemId?.let {
+            when (it) {
+                R.id.menu_settings -> {
+                    startActivity(Intent(this, SettingsActivity::class.java))
+                    true
+                }
+                else -> false
+            }
+        } ?: false
+    }
+}
