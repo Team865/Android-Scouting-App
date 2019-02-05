27d9b46ca50a4e760a94b63068384f994680b896
yuliu2016 <22801773+yuliu2016@users.noreply.github.com>
2019-02-04 23:49:08 -0500

    add toolbar to LicensesActivity

--- app/src/main/java/ca/warp7/android/scouting/LicensesActivity.kt
+++ app/src/main/java/ca/warp7/android/scouting/LicensesActivity.kt
@@ -8,19 +8,15 @@ import android.widget.TextView
 class LicensesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setTheme(R.style.SettingsTheme)
+        setTheme(R.style.AppTheme)
         setContentView(R.layout.activity_licenses)
         val text = findViewById<TextView>(R.id.text)
-        setupActionBar()
-        text.text = AppResources.getHTML(this, R.raw.licenses)
-    }
-
-    private fun setupActionBar() {
-        val actionBar = supportActionBar
-        if (actionBar != null) {
-            actionBar.setDisplayHomeAsUpEnabled(true)
-            actionBar.setTitle(R.string.open_source_licenses)
+        setSupportActionBar(findViewById(R.id.my_toolbar))
+        supportActionBar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            setTitle(R.string.open_source_licenses)
         }
+        text.text = AppResources.getHTML(this, R.raw.licenses)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
