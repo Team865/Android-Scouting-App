6d2e02a2bb5b700070776eb5dff127fc94929df1
yuliu2016 <22801773+yuliu2016@users.noreply.github.com>
2019-02-05 00:05:27 -0500

    add settings activity action bar

--- app/src/main/java/ca/warp7/android/scouting/SettingsActivity.kt
+++ app/src/main/java/ca/warp7/android/scouting/SettingsActivity.kt
@@ -2,6 +2,7 @@ package ca.warp7.android.scouting
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
 
 /**
  * @since v0.4.2
@@ -10,11 +11,26 @@ import android.support.v7.app.AppCompatActivity
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        supportFragmentManager.beginTransaction().replace(
-            android.R.id.content,
+        setContentView(R.layout.activity_settings)
+        setSupportActionBar(findViewById(R.id.my_toolbar))
+        supportActionBar?.apply {
+            title = "Scouting App Options"
+            setDisplayHomeAsUpEnabled(true)
+        }
+        supportFragmentManager.beginTransaction().add(
+            R.id.settings_frame,
             SettingsFragment()
         ).commit()
-        setTheme(R.style.SettingsTheme)
-        title = "Options"
+        setTheme(R.style.AppTheme)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                onBackPressed()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
