488c62c08e76a4be60935be3c352f3a6f9e4b2a8
yuliu2016 <22801773+yuliu2016@users.noreply.github.com>
2019-03-09 08:48:03 -0500

    release for Humber College Event

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         minSdkVersion(21)
         targetSdkVersion(28)
         versionCode = 1
-        versionName = "v2019.1.0-rc"
+        versionName = "v2019.1.0-humber"
         resConfigs("en", "hdpi")
     }
     buildTypes {
