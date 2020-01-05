208a6540db51cd8ecfd660ae6463b4ebc9b1339c
yuliu2016 <22801773+yuliu2016@users.noreply.github.com>
2020-01-05 14:41:00 -0500

    refactor: remove items from settings screen

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # Android-Scouting-App
 
 Match Scouting App for Android using QR codes to export data
-
-Screenshots are in the `img` folder
--- app/src/main/java/ca/warp7/android/scouting/SettingsFragment.kt
+++ app/src/main/java/ca/warp7/android/scouting/SettingsFragment.kt
@@ -1,5 +1,6 @@
 package ca.warp7.android.scouting
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.preference.PreferenceFragmentCompat
 
@@ -11,16 +12,13 @@ class SettingsFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preferences, rootKey)
 
-        val listener = SettingsClickListener()
-        addListener(R.string.pref_copy_assets_key, listener)
-        addListener(R.string.pref_v4_key, listener)
-        addListener(R.string.pref_licenses_key, listener)
+        findPreference(getString(R.string.pref_licenses_key)).setOnPreferenceClickListener {
+            val intent = Intent(context, LicensesActivity::class.java)
+            it.context.startActivity(intent)
+            true
+        }
 
         val aboutApp = findPreference(getString(R.string.pref_about_key))
         aboutApp.summary = "Version: " + BuildConfig.VERSION_NAME + "-" + BuildConfig.BUILD_TYPE
-    }
-
-    private fun addListener(id: Int, listener: SettingsClickListener) {
-        findPreference(getString(id)).onPreferenceClickListener = listener
     }
 }
