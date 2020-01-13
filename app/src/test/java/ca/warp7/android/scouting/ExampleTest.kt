b5e652de02b038641ed7f708bda57d6c7178244c
yuliu2016 <22801773+yuliu2016@users.noreply.github.com>
2020-01-13 17:41:34 -0500

    add junit test

---
+++ app/src/test/java/ca/warp7/android/scouting/ExampleTest.kt
@@ -0,0 +1,11 @@
+package ca.warp7.android.scouting
+
+import org.junit.Assert
+import org.junit.Test
+
+class ExampleTest {
+    @Test
+    fun testTrue() {
+        Assert.assertTrue(true)
+    }
+}
