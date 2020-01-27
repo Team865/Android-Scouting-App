c834350abc2c25a89b666e43efa68fef7c0692a1
yuliu2016 <22801773+yuliu2016@users.noreply.github.com>
2020-01-27 18:00:29 -0500

    Add Event Selection Activity

---
+++ app/src/main/java/ca/warp7/android/scouting/EventSelectionActivity.kt
@@ -0,0 +1,12 @@
+package ca.warp7.android.scouting
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class EventSelectionActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_event_selection)
+    }
+}
