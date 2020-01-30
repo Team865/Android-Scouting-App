cc7acb978c12a621a9b9e73536ab2952c63cfbbf
yuliu2016 <22801773+yuliu2016@users.noreply.github.com>
2020-01-29 20:49:42 -0500

    ZZZZZZZZZZZZZZZZZZZZZZZZZ

--- app/src/main/java/ca/warp7/android/scouting/ui/EntryInMatch.kt
+++ app/src/main/java/ca/warp7/android/scouting/ui/EntryInMatch.kt
@@ -9,4 +9,30 @@ class EntryInMatch(
     val isComplete: Boolean,
     val isScheduled: Boolean,
     val data: String = ""
-)
+) {
+
+    fun toCSV(): String {
+        val cols = ArrayList<Any>()
+        cols.add(match)
+        cols.addAll(teams)
+        cols.add(board)
+        cols.add(isComplete)
+        cols.add(isScheduled)
+        cols.add(data)
+        return cols.joinToString(",")
+    }
+
+    companion object {
+        fun fromCSV(s: String): EntryInMatch {
+            val sp = s.split(",")
+            return EntryInMatch(
+                sp[0],
+                sp.subList(1, 7).map { it.toInt() },
+                Board.valueOf(sp[7]),
+                sp[8].toBoolean(),
+                sp[9].toBoolean(),
+                sp[10]
+            )
+        }
+    }
+}
