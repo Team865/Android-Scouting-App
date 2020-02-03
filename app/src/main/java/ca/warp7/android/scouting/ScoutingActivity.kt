1bb22ddc56791a68143d9f0672e7ce2f5070d7d7
yuliu2016 <22801773+yuliu2016@users.noreply.github.com>
2020-02-03 17:34:38 -0500

    Add match schedule to super scouting encoder

--- app/src/main/java/ca/warp7/android/scouting/ScoutingActivity.kt
+++ app/src/main/java/ca/warp7/android/scouting/ScoutingActivity.kt
@@ -266,8 +266,16 @@ class ScoutingActivity : AppCompatActivity(), BaseScoutingActivity {
             RX, BX -> "ALL"
         }
 
+        // special entry team string to encode match schedule with super scouts
+        val entryTeam = when(board) {
+            RX -> teams.joinToString("_")
+            // invert the list order for the blue alliance
+            BX -> (teams.subList(3, 6) + teams.subList(0, 3)).joinToString("_")
+            else -> team
+        }
+
         this.entryInMatch = entryInMatch
-        entry = TimedEntry(match, team, scout, board, getCurrentTime().toInt()) { getRelativeTime() }
+        entry = TimedEntry(match, entryTeam, scout, board, getCurrentTime().toInt()) { getRelativeTime() }
 
         findViewById<TextView>(R.id.toolbar_match).text = match.let {
             val split = it.split("_")
