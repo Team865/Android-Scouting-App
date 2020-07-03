320840bba276d01ab6fc539ef756380f733f17d7
yuliu2016 <22801773+yuliu2016@users.noreply.github.com>
2020-07-03 14:59:02 -0400

    initial daynight parent style

--- app/src/main/java/ca/warp7/android/scouting/ui/SettingsFragment.kt
+++ app/src/main/java/ca/warp7/android/scouting/ui/SettingsFragment.kt
@@ -1,7 +1,10 @@
 package ca.warp7.android.scouting.ui
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.preference.ListPreference
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import androidx.preference.PreferenceManager
@@ -24,8 +27,24 @@ class SettingsFragment : PreferenceFragmentCompat() {
                     BuildConfig.VERSION_NAME + "-" + BuildConfig.BUILD_TYPE
         }
 
-        findPreference<Preference>(getString(R.string.pref_event_selection))?.setOnPreferenceClickListener {
+        val eventSelect = findPreference<Preference>(getString(R.string.pref_event_selection))
+        eventSelect?.setOnPreferenceClickListener {
             startActivity(Intent(context, EventSelectionActivity::class.java))
+            true
+        }
+
+        val darkTheme = findPreference<ListPreference>(getString(R.string.pref_dark_theme_key))
+        darkTheme?.setOnPreferenceChangeListener { _, newValue ->
+            val newState = when (newValue) {
+                "Light" -> AppCompatDelegate.MODE_NIGHT_NO
+                "Dark" -> AppCompatDelegate.MODE_NIGHT_YES
+                else -> if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                    AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
+                } else {
+                    AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY
+                }
+            }
+            AppCompatDelegate.setDefaultNightMode(newState)
             true
         }
     }
@@ -33,8 +52,8 @@ class SettingsFragment : PreferenceFragmentCompat() {
     override fun onResume() {
         super.onResume()
         val eventSelector = findPreference<Preference>(getString(R.string.pref_event_selection))
-        val preferences =  PreferenceManager
-            .getDefaultSharedPreferences(context)
+        val preferences = PreferenceManager
+                .getDefaultSharedPreferences(context)
         val event = preferences.getString(PreferenceKeys.kEventName, "No Event")
         val year = preferences.getString(PreferenceKeys.kYear, "")
         if (eventSelector != null) {
